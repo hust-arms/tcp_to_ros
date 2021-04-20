48027aa43809c0b2d13becab8ca6711321e48943
wangzhao9562 <wangzhao9562@gmail.com>
2021-04-20 23:42:16 +0800

    update test node

--- test/tcp_ros_bridge_test.cpp
+++ test/tcp_ros_bridge_test.cpp
@@ -13,18 +13,17 @@
 
 int main(int argc, char** argv)
 {
+    ros::init(argc, argv, "tcp_ros_bridge_test");
+
     if(argc != 3)
     {
         std::cerr << "[tcp_ros_bridge_test]: Usage: tcp_ros_bridge_test <auv_name> <port>" << std::endl;
         return -1;
     }
 
-    ros::init(argc, argv, "tcp_ros_bridge_test");
 
     boost::asio::io_service io_service;
     tcp_ros_bridge::uuv_tcp_ros_bridge bridge(argv[1], io_service, atoi(argv[2]));
-
-    io_service.run();
 
     ros::spin();
 
