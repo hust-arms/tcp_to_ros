a0c6336a7f431cf3c2ddaf08e236db18811a4439
wangzhao9562 <wangzhao9562@gmail.com>
2021-04-22 21:06:06 +0800

    Add node integrated with TCP server

---
+++ include/tcp_ros_bridge/tcp_ros_bridge_with_server.h
@@ -0,0 +1,435 @@
+/*                                                                          
+ * Filename: tcp_ros_bridge.h
+ * Path: tcp_ros_bridge
+ * Created Date: Saturday, Faburary 27th 2021, 15:14:39         
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2021 hust-arms
+ */
+
+#pragma once
+
+#include "message.h"
+
+#include <memory>
+
+#include <boost/thread/recursive_mutex.hpp>
+#include <boost/bind.hpp>
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/write.hpp>
+
+#include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/FluidPressure.h>
+#include <nav_msgs/Odometry.h>
+#include <uuv_sensor_ros_plugins_msgs/DVL.h>
+#include <uuv_gazebo_ros_plugins_msgs/FloatStamped.h>
+
+using boost::asio::ip::tcp;
+
+
+#define DEBUG_
+
+namespace tcp_ros_bridge{
+
+//----------------------------------------------------------------------
+
+class subscriber
+{
+public:
+  virtual ~subscriber() {}
+  virtual void deliver(uint8_t* msg, size_t bytes) = 0;
+};
+
+typedef boost::shared_ptr<subscriber> subscriber_ptr;
+
+//----------------------------------------------------------------------
+
+class channel
+{
+public:
+  void join(subscriber_ptr subscriber)
+  {
+    subscribers_.insert(subscriber);
+  }
+
+  void leave(subscriber_ptr subscriber)
+  {
+    subscribers_.erase(subscriber);
+  }
+
+  void deliver(uint8_t* msg, size_t bytes)
+  {
+    std::for_each(subscribers_.begin(), subscribers_.end(),
+        boost::bind(&subscriber::deliver, _1, boost::ref(msg), bytes));
+  }
+
+private:
+  std::set<subscriber_ptr> subscribers_;
+};
+
+//----------------------------------------------------------------------    
+
+const int BUFFER_MAX_LEN = 1024;
+
+class server;
+
+/**
+ * @brief transform messages from TCP client to ROS messages, use for UUV control
+ */
+class tcp_ros_bridge : 
+    public subscriber, 
+    public boost::enable_shared_from_this<tcp_ros_bridge>
+{
+    friend class server;
+public:
+    tcp_ros_bridge(const std::string& auv_name, boost::asio::io_service& io_service, channel& ch);
+    ~tcp_ros_bridge();
+
+private:
+    void start()
+    {
+      channel_.join(shared_from_this());
+    
+      std::cout << "[tcp_ros_bridge]: start read&write thread" << std::endl;
+      parse_th_ = new boost::thread(boost::bind(&tcp_ros_bridge::uuv_ctrl_publish_thread, this));
+      send_th_ = new boost::thread(boost::bind(&tcp_ros_bridge::uuv_status_send_thread, this));
+    }
+
+    tcp::socket& socket()
+    {
+      return socket_;
+    }
+
+    void stop()
+    {
+      channel_.leave(shared_from_this());
+  
+      socket_.close();
+  
+      std::cout << "[tcp_ros_bridge]: close server" << std::endl;
+    }
+
+    bool stopped() const
+    {
+      return !socket_.is_open();
+    }
+
+    /*
+     * @brief Parse UUV control message
+     */
+    void deliver(uint8_t* msg, size_t bytes) override
+    {
+      // std::cout << "[tcp_ros_bridge]: head of messsage: " << std::hex << *msg << std::endl;
+      // for(int i = 0; i < bytes; ++i)
+      // {
+      //    // parse
+      //    if(MESSAGE_FRAMING_OK == message_parse(*msg++))
+      //    {
+      //        uint8_t mode, status_fd, vel, light, elevator;
+      //        float course, depth;
+      //        
+      //        // get control parameters from command
+      //        message_get_control_cmd(&mode, &status_fd, &vel, &course, &depth, 
+      //                                &ctrl_info_.fin0_, &ctrl_info_.fin1_, &ctrl_info_.fin2_, &ctrl_info_.fin3_, &light, &elevator);
+      //
+      //        std::cout << "[tcp_ros_bridge]: recv fin: " << ctrl_info_.fin0_ << " "
+      //            << ctrl_info_.fin1_ << " "
+      //            << ctrl_info_.fin2_ << " "
+      //            << ctrl_info_.fin3_ << " " << std::endl;;
+      //
+      //        std::cout << "[tcp_ros_bridge]: thruster: " << vel << std::endl;
+      //    
+      //        ctrl_info_.rpm_ = static_cast<int>(vel) * 0.01 * 2300.0;
+      //    }
+      //    // else{
+      //    //    std::cout << "[tcp_ros_bridge]: parse message failed!" << std::endl;
+      //    // }
+      //}
+    }
+
+    void uuv_ctrl_publish_thread();
+
+    /**
+     * @param data send thread
+     */
+    void uuv_status_send_thread();
+
+    /**
+     * @param server manage thread
+     */
+    void server_manage_thread();
+
+    /**
+     * @param server check thread
+     */
+    void server_check_thread();
+
+    /**
+     * @param get control information from subscriber
+     */
+    void getCtrlInfo()
+    {
+        if(tcp_msg_sub_)
+            ctrl_info_ = tcp_msg_sub_->getCtrlInfo();
+    }
+
+    /* Sensors callback func */
+    /**
+     * @brief IMU data input
+     */
+    void imuCb(const sensor_msgs::Imu::ConstPtr& msg);
+
+    /**
+     * @brief Pressure sensor data input
+     */
+    void pressureCb(const sensor_msgs::FluidPressure::ConstPtr& msg);
+
+    /**
+     * @brief Position & pose input  
+     */ 
+    void posegtCb(const nav_msgs::Odometry::ConstPtr& msg); 
+    
+    /**
+     * @brief DVL input  
+     */ 
+    void dvlCb(const uuv_sensor_ros_plugins_msgs::DVL::ConstPtr& msg); 
+
+    /**
+     * @brief Upper port 
+     */
+    void fin0Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg);
+
+    /**
+     * @brief Upper starboard 
+     */
+    void fin1Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg);
+    
+    /**
+     * @brief Lower port 
+     */
+    void fin2Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg);
+    
+    /**
+     * @brief lower starboard 
+     */
+    void fin3Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg);
+
+    /**
+     * @brief thruster 
+     */
+    void thruster0Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg);
+
+private:
+    struct uuv_ctrl_info
+    {
+        uuv_ctrl_info()
+        {
+            fin0_ = 0.0; fin1_ = 0.0; fin2_ = 0.0; fin3_ = 0.0;
+            rpm_ = 0.0;
+        }
+
+        uuv_ctrl_info& operator=(const uuv_ctrl_info& ctrl_info)
+        {
+            if(this != &ctrl_info)
+            {
+                this->fin0_ = ctrl_info.fin0_;
+                this->fin1_ = ctrl_info.fin1_;
+                this->fin2_ = ctrl_info.fin2_;
+                this->fin3_ = ctrl_info.fin3_;
+                this->rpm_ = ctrl_info.rpm_;
+
+                return *this;
+            }
+        }
+
+        float fin0_;
+        float fin1_;
+        float fin2_;
+        float fin3_;
+
+        float rpm_;
+    }; // uuv_ctrl_info
+
+    struct uuv_status_info
+    {
+        double x_, y_, z_, u_, v_, w_;
+        double depth_;
+        double roll_, pitch_, yaw_, droll_, dpitch_, dyaw_;
+        double fin0_, fin1_, fin2_, fin3_; // up, us, lp, ls
+        double rpm_;
+
+        uuv_status_info()
+        {
+            x_ = 0.0; y_ = 0.0; z_ = 0.0; u_ = 0.0; v_ = 0.0; w_ = 0.0;
+            depth_ = 0.0;
+            roll_ = 0.0; pitch_ = 0.0; yaw_ = 0.0; droll_ = 0.0; dpitch_ = 0.0; dyaw_ = 0.0;
+            fin0_ = 0.0; fin1_ = 0.0; fin2_ = 0.0; fin3_ = 0.0;
+            rpm_ = 0.0;
+        }
+    }; // uuv_status_info
+
+    /**
+     * @brief TCP server subscriber for UUV control message
+     */
+    class uuv_ctrl_subscriber : public subscriber
+    {
+    public:
+        /**
+         * @brief Return control information
+         */
+        uuv_ctrl_info getCtrlInfo()const
+        {
+            return ctrl_info_;
+        }
+    
+        /**
+         * @brief Parse UUV control message
+         */
+        void deliver(uint8_t* msg, size_t bytes) override
+        {
+            std::cout << "[tcp_ros_bridge]: head of messsage: " << std::hex << *msg << std::endl;
+            for(int i = 0; i < bytes; ++i)
+            {
+               // parse
+               if(MESSAGE_FRAMING_OK == message_parse(*msg++))
+               {
+                   uint8_t mode, status_fd, vel, light, elevator;
+                   float course, depth;
+                   
+                   // get control parameters from command
+                   message_get_control_cmd(&mode, &status_fd, &vel, &course, &depth, 
+                                           &ctrl_info_.fin0_, &ctrl_info_.fin1_, &ctrl_info_.fin2_, &ctrl_info_.fin3_, &light, &elevator);
+    
+                   std::cout << "[tcp_ros_bridge]: recv fin: " << ctrl_info_.fin0_ << " "
+                       << ctrl_info_.fin1_ << " "
+                       << ctrl_info_.fin2_ << " "
+                       << ctrl_info_.fin3_ << " " << std::endl;;
+    
+                   std::cout << "[tcp_ros_bridge]: thruster: " << vel << std::endl;
+               
+                   ctrl_info_.rpm_ = static_cast<int>(vel) * 0.01 * 2300.0;
+               }
+               // else{
+               //    std::cout << "[tcp_ros_bridge]: parse message failed!" << std::endl;
+               // }
+           }
+        }
+    
+    private:
+        uuv_ctrl_info ctrl_info_;
+    }; // uuv_ctrl_subscriber 
+
+    typedef boost::shared_ptr<uuv_ctrl_subscriber> uuv_ctrl_sub_ptr;
+
+    tcp::socket socket_;
+    uint8_t buffer_[BUFFER_MAX_LEN];
+    channel& channel_;
+    // tcp::acceptor acceptor_;
+    // uuv_ctrl_sub_ptr tcp_msg_sub_;
+
+    std::string auv_name_;
+    int seq_;
+    double period_;
+
+    bool stopped_;
+    int port_; 
+    boost::asio::io_service& io_service_;
+
+    uuv_ctrl_sub_ptr tcp_msg_sub_;
+
+    boost::thread* parse_th_;
+    boost::thread* send_th_;
+    boost::thread* server_manage_th_;
+    boost::thread* server_check_th_;
+
+    boost::recursive_mutex uuv_ctrl_info_mutex_;
+    uuv_ctrl_info ctrl_info_;
+
+    boost::recursive_mutex uuv_status_info_mutex_;
+    uuv_status_info status_info_; 
+
+    ros::Publisher fins0_pub_; // upper port
+    ros::Publisher fins1_pub_; // upper starboard
+    ros::Publisher fins2_pub_; // lower port
+    ros::Publisher fins3_pub_; // lower starboard
+    ros::Publisher thruster0_pub_;
+    
+    ros::Subscriber imu_sub_, pressure_sub_, posegt_sub_, dvl_sub_;
+    ros::Subscriber fins0_sub_, fins1_sub_, fins2_sub_, fins3_sub_;
+    ros::Subscriber thruster0_sub_;
+}; // uuv_ros_to_tcp
+
+// ----------------------------------------------------------------------
+
+typedef boost::shared_ptr<tcp_ros_bridge> tcp_ros_bridge_ptr;
+
+class server
+{
+public:
+    server(const std::string& auv_name, boost::asio::io_service& io_service,
+         const tcp::endpoint& listen_endpoint)
+       : io_service_(io_service),
+         acceptor_(io_service, listen_endpoint),
+         auv_name_(auv_name)
+    {
+        acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+
+        std::cout << "[server]: Server initialize" << std::endl;
+        
+        start_accept();
+    }
+
+    void start_accept()
+    {
+      std::cout << "[server]: listen" << std::endl;
+      // tcp_session_ptr new_session(new tcp_session(io_service_, channel_));
+      session_ = boost::shared_ptr<tcp_ros_bridge>(new tcp_ros_bridge(auv_name_, io_service_, channel_));
+    
+      acceptor_.async_accept(session_->socket(),
+          boost::bind(&server::handle_accept, this, session_,
+                      boost::asio::placeholders::error));
+    }
+
+    void handle_accept(tcp_ros_bridge_ptr session,
+        const boost::system::error_code& ec)
+    {
+      if (!ec)
+      {
+        std::cout << "[server]: start tcp session" << std::endl;
+        session->start();
+    
+        start_accept();
+      }
+      else
+      {
+          std::cerr << "[server]: error in handle acceptance" << std::endl;
+      }
+    }
+
+    void stop_server()
+    {
+        session_->stop();
+    }
+
+private:
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+    channel channel_;
+    std::string auv_name_;
+
+    tcp_ros_bridge_ptr session_;
+
+}; // server
+}; // ns
+
---
+++ src/tcp_ros_bridge_with_server.cpp
@@ -0,0 +1,312 @@
+/*                                                                           
+ * Filename: tcp_ros_bridge.cpp
+ * Path: tcp_ros_bridge
+ * Created Date: Saturday, Faburary 27th 2021, 15:14:39         
+ * Author: zhao wang
+ * 
+ * Copyright (c) 2021 hust-arms
+ */
+
+
+#include "tcp_ros_bridge/tcp_ros_bridge_with_server.h"
+
+namespace tcp_ros_bridge
+{
+
+//////////////////////////////////
+tcp_ros_bridge::tcp_ros_bridge(const std::string& auv_name, 
+                               boost::asio::io_service& io_service, channel& ch) : 
+    auv_name_(auv_name), io_service_(io_service), socket_(io_service), channel_(ch)
+{
+    tcp_msg_sub_ = boost::shared_ptr<uuv_ctrl_subscriber>(new uuv_ctrl_subscriber());
+
+    message_init();
+
+    // initialize ROS components
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh("~");
+
+    private_nh.param("period", period_, 0.2);
+
+    thruster0_pub_= nh.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>(auv_name + "/thrusters/0/input", 1);
+    fins0_pub_ = nh.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>(auv_name + "/fins/0/input", 1);
+    fins1_pub_ = nh.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>(auv_name + "/fins/1/input", 1);
+    fins2_pub_ = nh.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>(auv_name + "/fins/2/input", 1);
+    fins3_pub_ = nh.advertise<uuv_gazebo_ros_plugins_msgs::FloatStamped>(auv_name + "/fins/3/input", 1);
+
+    imu_sub_ = nh.subscribe<sensor_msgs::Imu>(auv_name + "/imu", 1, boost::bind(&tcp_ros_bridge::imuCb, this, _1));
+    pressure_sub_ = nh.subscribe<sensor_msgs::FluidPressure>(auv_name + "/pressure", 1, boost::bind(&tcp_ros_bridge::pressureCb, this, _1));
+    posegt_sub_ = nh.subscribe<nav_msgs::Odometry>(auv_name + "/pose_gt", 1, boost::bind(&tcp_ros_bridge::posegtCb, this, _1));
+    dvl_sub_ = nh.subscribe<uuv_sensor_ros_plugins_msgs::DVL>(auv_name + "/dvl", 1, boost::bind(&tcp_ros_bridge::dvlCb, this, _1));
+
+
+    // try create async server
+    seq_ = 0;
+
+    stopped_ = false;
+
+    std::cout << "[tcp_ros_bridge]: start server" << std::endl;
+}
+
+//////////////////////////////////
+tcp_ros_bridge::~tcp_ros_bridge()
+{
+    if(server_check_th_ != nullptr)
+    {
+        server_check_th_->interrupt();
+        server_check_th_->join();
+        delete server_check_th_;
+        server_check_th_ = nullptr;
+    }
+
+    if(server_manage_th_ != nullptr)
+    {
+        server_manage_th_->interrupt();
+        server_manage_th_->join();
+        delete server_manage_th_;
+        server_manage_th_ = nullptr;
+    }
+
+    if(send_th_ != nullptr)
+    {
+        send_th_->interrupt();
+        send_th_->join();
+        delete send_th_;
+        send_th_ = nullptr;
+    }
+
+    if(parse_th_ != nullptr)
+    {
+        parse_th_->interrupt();
+        parse_th_->join();
+        delete parse_th_;
+        parse_th_ = nullptr;
+    }
+}
+
+//////////////////////////////////
+void tcp_ros_bridge::uuv_ctrl_publish_thread()
+{
+    ros::NodeHandle nh;
+
+    while(nh.ok())
+    {
+        double fins0, fins1, fins2, fins3, rpm;
+
+        boost::system::error_code ec;
+        
+        size_t bytes_read = socket_.read_some(boost::asio::buffer(buffer_, BUFFER_MAX_LEN), ec);
+
+        if(ec)
+        {
+            stop();
+            return;
+        }
+
+        tcp_msg_sub_->deliver(buffer_, bytes_read);
+
+        ctrl_info_ = tcp_msg_sub_->getCtrlInfo();
+        
+        fins0 = ctrl_info_.fin0_ / 57.3;
+        fins1 = ctrl_info_.fin1_ / 57.3;
+        fins2 = ctrl_info_.fin2_ / 57.3;
+        fins3 = ctrl_info_.fin3_ / 57.3;
+
+        rpm = ctrl_info_.rpm_;
+        
+
+        // publish control params
+        // thruster
+        std_msgs::Header header;
+        header.stamp.setNow(ros::Time::now());
+        header.frame_id = auv_name_ + "/base_link";
+        header.seq = ++seq_;
+
+        uuv_gazebo_ros_plugins_msgs::FloatStamped thrusters_msg;
+        thrusters_msg.header = header;
+        thrusters_msg.data = rpm;
+        thruster0_pub_.publish(thrusters_msg);
+
+        // fins
+        uuv_gazebo_ros_plugins_msgs::FloatStamped fins_msg;
+        fins_msg.header = header;
+        // upper port
+        fins_msg.data = fins0;
+        fins0_pub_.publish(fins_msg);
+        // upper starboard
+        fins_msg.data = fins1;
+        fins1_pub_.publish(fins_msg);
+        // lower port
+        fins_msg.data = fins2;
+        fins2_pub_.publish(fins_msg);
+        // lower starboard
+        fins_msg.data = fins3;
+        fins3_pub_.publish(fins_msg);
+
+        boost::this_thread::sleep(boost::posix_time::milliseconds(period_ * 1000));
+    }
+
+    stopped_ = true;
+}
+
+//////////////////////////////////
+void tcp_ros_bridge::uuv_status_send_thread()
+{
+    ros::NodeHandle nh;
+
+    while(nh.ok())
+    {
+        float x, y, z, u, v, w;
+        float roll, pitch, yaw, droll, dpitch, dyaw;
+        float fin0, fin1, fin2, fin3, rpm;
+        float ins_yaw;
+
+        {
+            boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+            x = status_info_.x_;
+            y = status_info_.y_;
+            z = status_info_.z_;
+            u = status_info_.u_;
+            v = status_info_.v_;
+            w = status_info_.w_;
+            roll = 57.3 * status_info_.roll_;
+            pitch = 57.3 * status_info_.pitch_;
+            yaw = 57.3 * status_info_.yaw_;
+            droll = 57.3 * status_info_.droll_;
+            dpitch = 57.3 * status_info_.dpitch_;
+            dyaw = 57.3 * status_info_.dyaw_;
+            fin0 = status_info_.fin0_;
+            fin1 = status_info_.fin1_;
+            fin2 = status_info_.fin2_;
+            fin3 = status_info_.fin3_;
+            rpm = status_info_.rpm_;
+        }
+
+        // std::cout << "[tcp_ros_bridge]: status: x: " << x << " y: " << y << " z: " << z << " u: "
+        //     << u << " v: " << v << " w: " << w << " roll: " << roll << " pitch: " << pitch << " yaw: " << yaw << " droll: "
+        //     << droll << " dpitch: " << dpitch << " dyaw: " << dyaw << " fin0: " << fin0 << " fin1: " << fin1 << " fin2: "
+        //     << fin2 << " fin3: " << fin3 << " rpm: " << rpm << std::endl;
+
+        ins_yaw = -yaw;
+
+        // [-pi, pi] -> [0, 2*pi]
+        if(ins_yaw < 0.0)
+            ins_yaw += 2 * 180.0;
+
+        // serialize 
+        uint8_t mode = 0xFF; uint8_t light = 0xFF; uint8_t elevator = 0xFF; // default
+        uint8_t cmd = 0xFF;
+
+        double lng = 114.31; double lat = 30.52;
+
+        message_status_pack(mode, 20.0, -5.0, -10.0, 0.0, 0.0, 0.0, 
+                            x, y, z, roll, pitch, yaw, 
+                            500.0 - abs(z), abs(z), 
+                            ins_yaw, -pitch, roll, -v, u, -w,
+                            lng, lat, 0.0, 0.0, 0.0, 
+                            fin0, fin1, fin2, fin3, cmd, 
+                            light, elevator);
+
+        uint8_t send_buffer[512];
+        uint16_t len = message_msg_to_send_buffer(send_buffer, 0);
+
+        // std::string msg(len, 0);
+        // for(size_t i = 0; i < len; ++i)
+        // {
+        //     msg[i] = static_cast<uint8_t>(send_buffer[i]);
+        // }
+
+        boost::system::error_code ec;
+
+        socket_.write_some(boost::asio::buffer(send_buffer, static_cast<size_t>(len)), ec);
+
+        if(ec)
+        {
+            stop();
+            return;
+        }
+
+        boost::this_thread::sleep(boost::posix_time::milliseconds(2 * period_ * 1000));
+    }
+}
+
+
+/////////////////////////////////////
+void tcp_ros_bridge::imuCb(const sensor_msgs::Imu::ConstPtr& msg) 
+{
+    // PoseStamped::Quaternion to tf::Quaternion
+    tf::Quaternion quat;
+    tf::quaternionMsgToTF(msg->orientation, quat);
+
+    // Quaternion to RPY
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    tf::Matrix3x3(quat).getRPY(status_info_.roll_, status_info_.pitch_, status_info_.yaw_);                                          
+
+    status_info_.droll_ = msg->angular_velocity.x;
+    status_info_.dpitch_ =  msg->angular_velocity.y;
+    status_info_.dyaw_ = msg->angular_velocity.z;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::pressureCb(const sensor_msgs::FluidPressure::ConstPtr& msg) // For pressure sensor
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.depth_ = static_cast<double>((msg->fluid_pressure - 101) / 10.1) - 0.25;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::posegtCb(const nav_msgs::Odometry::ConstPtr& msg) // For pose sensor
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.x_ = msg->pose.pose.position.x;
+    status_info_.y_ = msg->pose.pose.position.y;
+    status_info_.z_ = msg->pose.pose.position.z;
+    // std::cout << "[tcp_ros_bridge]: pose_gt cb: " << status_info_.x_ << " " 
+    //     << status_info_.y_ << " " << status_info_.z_ << std::endl;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::dvlCb(const uuv_sensor_ros_plugins_msgs::DVL::ConstPtr& msg) // For DVL
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.u_ = msg->velocity.x;
+    status_info_.v_ = msg->velocity.y;
+    status_info_.w_ = msg->velocity.z;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::fin0Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg)
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.fin0_ = msg->data;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::fin1Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg)
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.fin1_ = msg->data;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::fin2Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg)
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.fin2_ = msg->data;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::fin3Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg)
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.fin3_ = msg->data;
+}
+
+/////////////////////////////////////
+void tcp_ros_bridge::thruster0Cb(const uuv_gazebo_ros_plugins_msgs::FloatStamped::ConstPtr& msg)
+{
+    boost::unique_lock<boost::recursive_mutex> lock(uuv_status_info_mutex_);
+    status_info_.rpm_ = msg->data;
+}
+
+}; // ns
